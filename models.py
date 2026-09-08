497cb95df9b9ff17d60ccbeaf8922f7d9ba8ed03
Ago-el <107497021+Ago-el@users.noreply.github.com>
2026-09-08 20:51:04 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-fromuid
+from uuid import uuid4
 from datetime import datetime, date
 from sqlalchemy import String, Text, Boolean, Date, DateTime, ForeignKey, UniqueConstraint, JSON, func
 from sqlalchemy.orm import Mapped, mapped_column
